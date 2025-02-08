9ac559e66a17d04e10ecf8a7253999bdaec8bb90
Praniti  Kubal <140830559+Praniti1594@users.noreply.github.com>
2025-02-08 11:31:09 +0530

    config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 DB_USERNAME = "root"
-DB_PASSWORD = "nnps#1594"
+DB_PASSWORD = ""
 DB_NAME = "encryption_db"
 DB_HOST = "localhost"
 
-SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:nnps#1594@localhost:3306/encryption_db"
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://username:pswd@localhost:3306/encryption_db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "supersecretkey"
